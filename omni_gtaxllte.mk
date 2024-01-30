a3dcc5fd3b44ca023175f663b4548ea9db5c23d3
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-01-30 22:36:52 +0200

    Update omni_gtaxllte.mk

--- omni_gtaxllte.mk
+++ omni_gtaxllte.mk
@@ -1,7 +1,6 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit from our custom product configuration
+$(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
